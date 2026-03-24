32b880bca83f2c16cea3afc32e9427e760e1cae8
juniorbonini <juniorboninia7x@gmail.com>
2026-03-24 12:50:24 -0300

    feat: adiciona entidade Post para estruturação dos dados

---
+++ src/models/post/post-model.ts
@@ -0,0 +1,12 @@
+export interface Post {
+  id: string;
+  title: string;
+  excerpt: string;
+  slug: string;
+  content: string;
+  coverImageUrl: string;
+  published: boolean;
+  createdAt: string;
+  updatedAt: string;
+  author: string;
+}
